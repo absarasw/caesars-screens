ee5d7c3c2adf44b11db1977b9218116740256264
Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro-6.local>
2023-05-06 20:21:28 +0530

    Adding scripts

--- scripts/menu-builder.js
+++ scripts/menu-builder.js
@@ -1,101 +1,119 @@
 /**
- *
+ * this method generates the HTML structure of the Menu layout
  * @param rootDocument
  * @returns {Promise<void>}
  */
-export const layout = async function createMenusFlexLayout(rootDocument) {
-    const beveragesFirstSectionSelector = '.section.beverages-heading';
-    const beveragesFirstSectionElement = rootDocument.querySelector(beveragesFirstSectionSelector);
+export const layout = async function createMenusHtmlLayout(rootDocument) {
+  const beveragesFirstSectionSelector = '.section.beverages-heading';
+  const beveragesFirstSectionElement = rootDocument.querySelector(
+    beveragesFirstSectionSelector,
+  );
 
-    const beveragesSecondSectionSelector = '.section.beverages-content';
-    const beveragesSecondSectionElement = rootDocument.querySelector(beveragesSecondSectionSelector);
+  const beveragesSecondSectionSelector = '.section.beverages-content';
+  const beveragesSecondSectionElement = rootDocument.querySelector(
+    beveragesSecondSectionSelector,
+  );
 
-    if (beveragesFirstSectionElement && beveragesSecondSectionElement) {
-        // create a new root div element for Beverages Menu
-        const beveragesMenu = document.createElement("div");
-        beveragesMenu.className = 'beverages-menu';
-        rootDocument.querySelector(`main`).append(beveragesMenu);
+  if (beveragesFirstSectionElement && beveragesSecondSectionElement) {
+    // create a new root div element for Beverages Menu
+    const beveragesMenu = document.createElement('div');
+    beveragesMenu.className = 'beverages-menu';
+    rootDocument.querySelector('main').append(beveragesMenu);
 
-        beveragesMenu.appendChild(beveragesFirstSectionElement);
-        beveragesMenu.appendChild(beveragesSecondSectionElement);
-    }
+    beveragesMenu.appendChild(beveragesFirstSectionElement);
+    beveragesMenu.appendChild(beveragesSecondSectionElement);
+  }
 
-    const foodFirstSectionSelector = '.section.sweets';
-    const foodFirstSectionElement = rootDocument.querySelector('.section.sweets');
+  const foodFirstSectionSelector = '.section.sweets';
+  const foodFirstSectionElement = rootDocument.querySelector(
+    foodFirstSectionSelector,
+  );
 
-    const foodSecondSectionSelector = '.section.brioche-savory';
-    const foodSecondSectionElement = rootDocument.querySelector('.section.brioche-savory');
+  const foodSecondSectionSelector = '.section.brioche-savory';
+  const foodSecondSectionElement = rootDocument.querySelector(
+    foodSecondSectionSelector,
+  );
 
-    const foodThirdSectionSelector = '.section.sides';
-    const foodThirdSectionElement = rootDocument.querySelector('.section.sides');
+  const foodThirdSectionSelector = '.section.sides';
+  const foodThirdSectionElement = rootDocument.querySelector(
+    foodThirdSectionSelector,
+  );
 
-    if (foodFirstSectionElement && foodSecondSectionElement && foodThirdSectionElement) {
-        // create a new root div element for Beverages Menu
-        const foodMenu = document.createElement("div");
-        foodMenu.className = 'food-menu';
-        rootDocument.querySelector(`main`).append(foodMenu);
+  if (
+    foodFirstSectionElement
+    && foodSecondSectionElement
+    && foodThirdSectionElement
+  ) {
+    // create a new root div element for Beverages Menu
+    const foodMenu = document.createElement('div');
+    foodMenu.className = 'food-menu';
+    rootDocument.querySelector('main').append(foodMenu);
 
-        foodMenu.appendChild(foodFirstSectionElement);
-        foodMenu.appendChild(foodSecondSectionElement);
-        foodMenu.appendChild(foodThirdSectionElement);
-    }
-
-}
+    foodMenu.appendChild(foodFirstSectionElement);
+    foodMenu.appendChild(foodSecondSectionElement);
+    foodMenu.appendChild(foodThirdSectionElement);
+  }
+};
 
 /**
- * ToDo
+ * This method creates nested html elements to place grid
+ * & flex items in their respective containers
  * @param rootDocument
  * @returns {Promise<void>}
  */
-export const nestedTable = async function createAlcoholBevarageNestedTable(rootDocument) {
-    const rootSelector = '.section.beverages-content';
-    const beverageContentDocumentRoot = rootDocument.querySelector(rootSelector);
+export const nestedTable = async function createAlcoholBevarageNestedTable(
+  rootDocument,
+) {
+  const rootSelector = '.section.beverages-content';
+  const beverageContentDocumentRoot = rootDocument.querySelector(rootSelector);
 
-    if (!beverageContentDocumentRoot) {
-        return;
-    }
+  if (!beverageContentDocumentRoot) {
+    return;
+  }
 
-    const tableHeading = beverageContentDocumentRoot.querySelector(`${rootSelector} > div:nth-last-child(-n + 4)`);
-    const wineTable = beverageContentDocumentRoot.querySelector(`${rootSelector} > div:nth-last-child(-n + 3)`);
-    const champagneTable = beverageContentDocumentRoot.querySelector(`${rootSelector} > div:nth-last-child(-n + 2)`);
-    const beerTable = beverageContentDocumentRoot.querySelector(`${rootSelector} > div:nth-last-child(-n + 1)`);
+  const coffeeTable = beverageContentDocumentRoot.querySelector(
+    `${rootSelector} > div:nth-last-child(-n + 5)`,
+  );
+  const alcoholicBeverageTableHeading = beverageContentDocumentRoot.querySelector(
+    `${rootSelector} > div:nth-last-child(-n + 4)`,
+  );
+  const wineTable = beverageContentDocumentRoot.querySelector(
+    `${rootSelector} > div:nth-last-child(-n + 3)`,
+  );
+  const champagneTable = beverageContentDocumentRoot.querySelector(
+    `${rootSelector} > div:nth-last-child(-n + 2)`,
+  );
+  const beerTable = beverageContentDocumentRoot.querySelector(
+    `${rootSelector} > div:nth-last-child(-n + 1)`,
+  );
 
-    // create a new nested table div element
-    const alcoholBeverageNestedTableDiv = document.createElement("div");
-    alcoholBeverageNestedTableDiv.className = 'alcohol-beverages-table';
-    rootDocument.querySelector(`${rootSelector}`).append(alcoholBeverageNestedTableDiv);
+  // create a new parents table div for top section
+  const beverageContentCoffeeTableDiv = document.createElement('div');
+  beverageContentCoffeeTableDiv.className = 'beverages-content-coffee';
+  rootDocument
+    .querySelector(`${rootSelector}`)
+    .append(beverageContentCoffeeTableDiv);
+  beverageContentCoffeeTableDiv.append(coffeeTable);
 
-    // create a new nested table div element
-    const wineChampagneNestedTableDiv = document.createElement("div");
-    wineChampagneNestedTableDiv.className = 'wine-champagne-left-table';
-    wineChampagneNestedTableDiv.appendChild(wineTable);
-    wineChampagneNestedTableDiv.appendChild(champagneTable);
+  // create a new parents table div for bottom section
+  const beverageContentAlcoholTableDiv = document.createElement('div');
+  beverageContentAlcoholTableDiv.className = 'beverages-content-alcohol';
+  rootDocument
+    .querySelector(`${rootSelector}`)
+    .append(beverageContentAlcoholTableDiv);
 
-    alcoholBeverageNestedTableDiv.appendChild(tableHeading);
-    alcoholBeverageNestedTableDiv.appendChild(wineChampagneNestedTableDiv);
-    alcoholBeverageNestedTableDiv.appendChild(beerTable);
+  // create a new nested table div element
+  const alcoholBeverageNestedTableDiv = document.createElement('div');
+  alcoholBeverageNestedTableDiv.className = 'alcohol-beverages-table';
+  beverageContentAlcoholTableDiv.append(alcoholBeverageNestedTableDiv);
 
-}
+  // create a new nested table div element
+  const wineChampagneNestedTableDiv = document.createElement('div');
+  wineChampagneNestedTableDiv.className = 'wine-champagne-left-table';
+  wineChampagneNestedTableDiv.appendChild(wineTable);
+  wineChampagneNestedTableDiv.appendChild(champagneTable);
 
-/**
- *
- * @param rootDocument
- * @returns {Promise<void>}
- */
-export const hidePlaceholders = async function findAndHideTemplatePlaceholders(rootDocument, elementsMap) {
-
-    const startsWithTemplateLiteral = '{{';
-    const endsWithTemplateLiteral = '}}';
-
-    // Find all the HTML elements whose text content matches the regular expression
-    const matchedElements = [];
-    const elements = document.getElementsByTagName('div');
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
-        if (!element.querySelector('div') && element.textContent.includes(startsWithTemplateLiteral) && element.textContent.includes(endsWithTemplateLiteral)) {
-            elementsMap.set(element.textContent.substring(element.textContent.indexOf(startsWithTemplateLiteral), element.textContent.indexOf(endsWithTemplateLiteral) + 2), element);
-            element.style.display = 'none';
-        }
-    }
-
-}
+  alcoholBeverageNestedTableDiv.appendChild(alcoholicBeverageTableHeading);
+  alcoholBeverageNestedTableDiv.appendChild(wineChampagneNestedTableDiv);
+  alcoholBeverageNestedTableDiv.appendChild(beerTable);
+};
